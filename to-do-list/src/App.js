02924f896c231dba7dc473487bb75e57b0f68ffb
rachelmaria09 <rmgildea@gmail.com>
2020-07-30 22:10:39 -0400

    first

---
+++ to-do-list/src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function App() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <p>Text Here</p>
+
+            <input type="checkbox" />
+            <p>Text Here</p>
+
+            <input type="checkbox" />
+            <p>Text Here</p>
+        </div>
+    )
+}
+
+export default App
