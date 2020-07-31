9381da2027e2ed93701131253ad1a0f7e73b664f
rachelmaria09 <rmgildea@gmail.com>
2020-07-30 23:19:25 -0400

    four

---
+++ week1/mytodolist/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input type="checkbox" />
+      <p>Grocery Shop</p>
+
+      <input type="checkbox" />
+      <p>Mow lawn</p>
+
+      <input type="checkbox" />
+      <p>Laundry</p>
+
+      <input type="checkbox" />
+      <p>Vacuum</p>
+    </div>
+  );
+}
+
+export default App;
