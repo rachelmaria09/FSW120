770c28c5d5a4ad1d838e7a223c9864182fcc8ee1
rachelmaria09 <rmgildea@gmail.com>
2020-08-07 16:11:13 -0400

    third

--- week2/coloredinfoboxes/src/index.js
+++ week2/coloredinfoboxes/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Movieinfo from './Movieinfo';
 
 ReactDOM.render(
   <React.StrictMode>
