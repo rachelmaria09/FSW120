97dedcf12a453fc323a59de22ff6262d0c425dab
rachelmaria09 <rmgildea@gmail.com>
2020-08-20 15:35:49 -0400

    final

--- week3/rollingdice/src/App.js
+++ week3/rollingdice/src/App.js
@@ -23,9 +23,15 @@ diceRoll = () => {
   this.setState(prevState => {
     numOne = (prevState.num1 * 0) + (Math.floor(Math.random()*6)+1)
     numTwo = (prevState.num2 * 0) + (Math.floor(Math.random()*6)+1)
+    numThree = (prevState.num3 * 0) + (Math.floor(Math.random()*6)+1)
+    numFour = (prevState.num4 * 0) + (Math.floor(Math.random()*6)+1)
+    numFive = (prevState.num5 * 0) + (Math.floor(Math.random()*6)+1)
     return{
       num1: numOne,
-      num2: numTwo
+      num2: numTwo,
+      num3: numThree,
+      num4: numFour,
+      num5: numFive
   }})}
  render() {
    return (
@@ -34,6 +40,9 @@ diceRoll = () => {
        <div className = "buttons">
        <DiceBox rolledDice={this.state.num1}/>
        <DiceBox rolledDice={this.state.num2}/>
+       <DiceBox rolledDice={this.state.num3}/>
+       <DiceBox rolledDice={this.state.num4}/>
+       <DiceBox rolledDice={this.state.num5}/>
        <button onClick={this.diceRoll}>Roll!</button>
      </div>
      </div>
